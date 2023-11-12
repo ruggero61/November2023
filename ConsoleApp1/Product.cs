563e57081161b0572a571477637e7096558583e4
ruggero61 <ruggerosoftwareengineer@gmail.com>
2023-11-12 14:46:36 +0100

    first deploy empty Program, Product and ProductList classes added

---
+++ ConsoleApp1/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp1
+{
+  public class Product
+  {
+    private string _name;
+    private decimal _price;
+    private int _stock;
+    private string _productName;
+    private string _productDescription;
+  }
+}
